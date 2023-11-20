56991dc7267e0070e444e424d004ce9e1962a574
exquilla98 <dinukamalshan14@gmail.com>
2023-11-20 09:58:06 +0530

    first commit

---
+++ folkart/src/main/java/com/folkart/folkart/FolkartApplication.java
@@ -0,0 +1,13 @@
+package com.folkart.folkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolkartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolkartApplication.class, args);
+	}
+
+}
---
+++ folkart/src/test/java/com/folkart/folkart/FolkartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folkart.folkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolkartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
